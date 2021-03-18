bf3cd6f1064fa84067b9d1f8f349fcc450d15a1f
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-18 12:41:19 +0100

    active.js import and export setup

---
+++ js/components/active.js
@@ -0,0 +1,5 @@
+const active = function() {
+  console.log ('active function working');
+}
+
+export { active };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+import { active } from './components/active.js';
 import { dsiplayCookieNotcie } from './components/cookie-notice.js';
 import { hideNavbar } from './components/navbar-hide.js';
 import { insertNews } from './components/news.js';
 
+active();
 window.onscroll = hideNavbar;
 insertNews();
 dsiplayCookieNotcie();
