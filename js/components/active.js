86a3f92a5af7346987f5c83986e4403154228dc9
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-18 12:59:57 +0100

    active.js sort of works

--- js/components/active.js
+++ js/components/active.js
@@ -1,5 +1,14 @@
 const active = function() {
-  console.log ('active function working');
+  const nav = document.querySelector('nav');
+  const li = nav.querySelectorAll('li');
+
+  for (var i = 0; i < li.length; i++) {
+    li[i].addEventListener('click', function {
+      var current = document.getElementsByClassName('active');
+      current[0].className = current[0].className.replace('active', '');
+      this.className += 'active';
+    })
+  }
 }
 
 export { active };
