c5d5ecba745bc73f0436f6e7451b81c29dc7fc00
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-23 21:22:52 +0100

    changed displayed url by setting the virtual path prefix to '/' && setthe url extions to display without '.html'

--- hundeakademie_nodeapp/app.js
+++ hundeakademie_nodeapp/app.js
@@ -2,3 +2,6 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening to Port 3000'));
 app.use(express.static('../'));
+app.use('/', express.static('../pages', {
+  extensions: ['html']
+}));
