27e8846166d01c9c79e5c25975dcf2bebbadf0ed
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-27 14:35:59 +0100

    layout reworked: article (news) inside banner + responsiveness && added @media(max-width: 400px;)

--- js/components/insert-news.js
+++ js/components/insert-news.js
@@ -1,8 +1,15 @@
 function insertNews() {
   if (window.location.pathname === '/') {
-    document.getElementById('news-header').innerHTML = "Ich bin eine eingefügte Überschrift";
-    document.getElementById('news-p').innerHTML = "Ich bin ein eingefügter Paragraph"
+    document.getElementById('news-header').innerHTML = "Corona- Zeiten";
+    document.getElementById('news-p').innerHTML = "Ich Liebe Hundebesitzer und Hundebesitzerinnen, Liebe Kunden <br>um weiterhin die Versorgung Ihrer Tiere zu gewährleisten, bleibt meine Praxis geöffnet.<br>Bitte kontaktieren Sie mich telefonisch oder per Email, damit wir die Termine sicher und unter Beachtung der gebotenen Maßnahmen vereinbaren können.<br>Ich wünsche Ihnen starke Nerven für diese schwere Zeit<br>Bleiben Sie und Ihre Tiere gesund<br>Nina Bayer"
   }
 };
+
+// <h3 id="news-header">
+// <!-- Corona- Zeiten -->
+// </h3>
+// <p id="news-p">
+// <!-- Liebe Hundebesitzer und Hundebesitzerinnen, Liebe Kunden <br>um weiterhin die Versorgung Ihrer Tiere zu gewährleisten, bleibt meine Praxis geöffnet.<br>Bitte kontaktieren Sie mich telefonisch oder per Email, damit wir die Termine sicher und unter Beachtung der gebotenen Maßnahmen vereinbaren können.<br>Ich wünsche Ihnen starke Nerven für diese schwere Zeit<br>Bleiben Sie und Ihre Tiere gesund<br>Nina Bayer -->
+// </p>
 
 export { insertNews };
