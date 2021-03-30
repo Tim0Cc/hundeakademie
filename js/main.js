8c770887622d88e17e33c4767a56421406442181
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-30 15:31:03 +0200

    fetch for cookieNotice works

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 console.log('main.js beginning')
 
 import { hideNavbar } from './components/hide-navbar-onscroll.js';
-// import { displayCookieNotice } from './components/hide-cookie-notice.js';
+import { displayCookieNotice } from './components/hide-cookie-notice.js';
 // import { active } from './components/active-menu.js';
 // import { insertNews } from './components/insert-news.js';
 
 window.onscroll = hideNavbar;
-// displayCookieNotice();
+displayCookieNotice();
 
 // console.log('main.js before calling active()')
 // active();
