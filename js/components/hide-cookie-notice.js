650173d3890224cd197021a630ec9b318e6108da
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-30 18:24:58 +0200

    both images of Nina updated

--- js/components/hide-cookie-notice.js
+++ js/components/hide-cookie-notice.js
@@ -1,12 +1,15 @@
+
 const hideCookieNotice = function(event) {
-  const cookieNotice = document.getElementById('cookie-notice')
-  const cookieButton = document.getElementById('cookie-button')
-  if (cookieNotice !== null && cookieButton !== null) {
-    cookieNotice.style.bottom = "0";
-    cookieButton.addEventListener('click', function() {
-      cookieNotice.style.bottom = "-64px";
-    });
-  }
+  if (document.getElementById('cookie-notice-placeholder') !== null) {
+    const cookieNotice = document.getElementById('cookie-notice')
+    const cookieButton = document.getElementById('cookie-button')
+    if (cookieNotice !== null && cookieButton !== null) {
+      cookieNotice.style.bottom = "0";
+      cookieButton.addEventListener('click', function() {
+        cookieNotice.style.bottom = "-64px";
+      });
+    }
+  };
 }
 
 export { hideCookieNotice };
