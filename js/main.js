f262f88caa93b72f3a49a91ec0f3b91078d152cb
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-30 15:17:12 +0200

    fetch for navbar && navbar-hide.js works

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-// console.log('main.js beginning')
+console.log('main.js beginning')
 
-// import { hideNavbar } from './components/hide-navbar-onscroll.js';
-// import { displayCookieNotice } from './components/hide-cookie-notice.js';
-// import { active } from './components/active-menu.js';
-// import { insertNews } from './components/insert-news.js';
+import { hideNavbar } from './components/hide-navbar-onscroll.js';
+import { displayCookieNotice } from './components/hide-cookie-notice.js';
+import { active } from './components/active-menu.js';
+import { insertNews } from './components/insert-news.js';
 
-// window.onscroll = hideNavbar;
-// displayCookieNotice();
+window.onscroll = hideNavbar;
+displayCookieNotice();
 
-// console.log('main.js before calling active()')
-// active();
-// console.log('main.js after calling active()')
+console.log('main.js before calling active()')
+active();
+console.log('main.js after calling active()')
 
-// insertNews();
+insertNews();
