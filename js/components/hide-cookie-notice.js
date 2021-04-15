3b05e86c20603eae9da9d3f6b054cef1e364f5db
Tim Cecatka <tim.cecatka@gmail.com>
2021-04-15 21:27:38 +0200

    commented out all console.logs in hide-cookie-notice.js

--- js/components/hide-cookie-notice.js
+++ js/components/hide-cookie-notice.js
@@ -1,12 +1,12 @@
 
-console.log('hide-cookie-notice.js');
+// console.log('hide-cookie-notice.js');
 function hideCookieNotice(event) {
-  console.log('const hideCookieNotice')
+  // console.log('const hideCookieNotice')
   // if (document.getElementById('cookie-notice-placeholder') !== null) {
   //   console.log('if statement');
     const cookieNotice = document.getElementById('cookie-notice')
     const cookieButton = document.getElementById('cookie-button')
-    console.log(cookieNotice + ' ' + cookieButton);
+    // console.log(cookieNotice + ' ' + cookieButton);
     if (cookieNotice !== null && cookieButton !== null) {
       console.log('2nd if statement');
       cookieNotice.style.bottom = "0";
@@ -15,8 +15,8 @@ function hideCookieNotice(event) {
       });
     }
   // };
-  console.log('const end');
+  // console.log('const end');
 }
-console.log('js end');
+// console.log('js end');
 
 export { hideCookieNotice };
