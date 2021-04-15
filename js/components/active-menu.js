e0d85b7c4a69509343500417f4951fd7782e3638
Tim Cecatka <tim.cecatka@gmail.com>
2021-04-15 23:17:17 +0200

    trying to fix active navbar

--- js/components/active-menu.js
+++ js/components/active-menu.js
@@ -1,12 +1,13 @@
-console.log('active-menu.js beginning')
+console.log('active-menu.js beginning');
+const nav = document.getElementById('navList');
 function active() {
-  console.log('active-menu.js active(){} beginning')
-  const nav = document.getElementById('navList')
-  console.log('active-menu.js active(){} after initializing nav')
+  console.log('active-menu.js active(){} beginning');
+  console.log(nav);
+  console.log('active-menu.js active(){} after initializing nav');
   if (nav !== null) {
-    console.log('active-menu.js active(){} inside the if statement')
+    console.log('active-menu.js active(){} inside the if statement');
     nav.addEventListener('click', function(event) {
-      console.log('active-menu.js active(){} inside the if statement in the eventlistener')
+      console.log('active-menu.js active(){} inside the if statement in the eventlistener');
       
       var activeLi = nav.querySelectorAll('.active');
       console.log(activeLi);
@@ -16,8 +17,8 @@ function active() {
       event.target.className = "active";
     })
   }
-  console.log('active-menu.js active(){} end')
+  console.log('active-menu.js active(){} end');
 }
-console.log('active-menu.js active(){} after')
+console.log('active-menu.js active(){} after');
 
-export { active };
+export { nav, active };
--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ console.log('main.js beginning')
 import { loadCommon } from './components/load-common.js'
 import { hideNavbar } from './components/hide-navbar-onscroll.js';
 import { hideCookieNotice } from './components/hide-cookie-notice.js';
-// import { active } from './components/active-menu.js';
+import { nav, active } from './components/active-menu.js';
 import { insertNews } from './components/insert-news.js';
+
 
 loadCommon();
 window.onscroll = hideNavbar;
@@ -14,5 +15,7 @@ hideCookieNotice();
 insertNews();
 
 // console.log('main.js before calling active()')
-// active();
+nav;
+console.log(nav);
+active();
 // console.log('main.js after calling active()')
